6aea910886533bb0c4b80e5490768b6c54e70561
parcellaann <anna.betik@mail.ru>
2023-01-28 19:41:05 +0300

    add solution

--- Homework 5.6/Homework 5.6.cpp
+++ Homework 5.6/Homework 5.6.cpp
@@ -1,20 +1,84 @@
 ﻿// Homework 5.6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int rows = 1, columns = 1;
+	std::cout << "Enter the number of rows:\n";
+	std::cin >> rows;
+	std::cout << "Enter the number of columns:\n";
+	std::cin >> columns;
+	int arr[100][100];
+	std::srand(std::time(0));
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			arr[i][j] = rand() % (10);
+		}
+
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	std::cout << "\n\nTask 1:\n";
+
+	for (int i = 0; i < rows; i++)
+	{
+		bool flag = true;
+		while (flag)
+		{
+			flag = false;
+			for (int j = 0; j < columns - 1; j++)
+			{
+				if (arr[i][j] > arr[i][j + 1])
+				{
+					flag = true;
+					int z = arr[i][j];
+					arr[i][j] = arr[i][j + 1];
+					arr[i][j + 1] = z;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	std::cout << "\n\nTask 2:\n";
+
+	for (int i = 0; i < rows; i++)
+	{
+		int z = arr[i][0];
+		arr[i][0] = arr[i][columns - 1];
+		arr[i][columns - 1] = z;
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
